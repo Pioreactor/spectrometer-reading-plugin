fa7c1da693ea07e88a9f5056d815d8ac21385d85
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-10-01 16:27:55 -0400

    adding pre-commit, and config for led current

--- spectrometer_reading_plugin/spectrometer_reading.py
+++ spectrometer_reading_plugin/spectrometer_reading.py
@@ -5,19 +5,16 @@ import adafruit_as7341
 import board
 import click
 from pioreactor.background_jobs.base import BackgroundJobWithDodging
+from pioreactor.background_jobs.leader.mqtt_to_db_streaming import produce_metadata
+from pioreactor.background_jobs.leader.mqtt_to_db_streaming import TopicToParserToTable
+from pioreactor.config import config
+from pioreactor.utils.timing import current_utc_timestamp
 from pioreactor.whoami import get_latest_experiment_name
 from pioreactor.whoami import get_unit_name
-from pioreactor.utils.timing import current_utc_timestamp
-
-from pioreactor.background_jobs.leader.mqtt_to_db_streaming import TopicToParserToTable
-from pioreactor.background_jobs.leader.mqtt_to_db_streaming import produce_metadata
-from pioreactor.background_jobs.leader.mqtt_to_db_streaming import msgspec_loads
-
 
 
 def parser(topic, payload) -> dict:
     metadata = produce_metadata(topic)
-    rpms = msgspec_loads(payload)
 
     return {
         "experiment": metadata.experiment,
@@ -28,25 +25,26 @@ def parser(topic, payload) -> dict:
     }
 
 
-topic_to_parser_to_table_spectrum = TopicToParserToTable([
-    "pioreactor/+/+/spectrometer_reading/band_415",
-    "pioreactor/+/+/spectrometer_reading/band_445",
-    "pioreactor/+/+/spectrometer_reading/band_480",
-    "pioreactor/+/+/spectrometer_reading/band_515",
-    "pioreactor/+/+/spectrometer_reading/band_555",
-    "pioreactor/+/+/spectrometer_reading/band_590",
-    "pioreactor/+/+/spectrometer_reading/band_630",
-    "pioreactor/+/+/spectrometer_reading/band_680",
+topic_to_parser_to_table_spectrum = TopicToParserToTable(
+    [
+        "pioreactor/+/+/spectrometer_reading/band_415",
+        "pioreactor/+/+/spectrometer_reading/band_445",
+        "pioreactor/+/+/spectrometer_reading/band_480",
+        "pioreactor/+/+/spectrometer_reading/band_515",
+        "pioreactor/+/+/spectrometer_reading/band_555",
+        "pioreactor/+/+/spectrometer_reading/band_590",
+        "pioreactor/+/+/spectrometer_reading/band_630",
+        "pioreactor/+/+/spectrometer_reading/band_680",
     ],
     parser,
-    "as7341_spectrum_readings"
+    "as7341_spectrum_readings",
 )
 
 
 class SpectrometerReading(BackgroundJobWithDodging):
 
-    job_name='spectrometer_reading'
-    
+    job_name = "spectrometer_reading"
+
     published_settings = {
         "band_415": {"datatype": "float", "unit": "AU", "settable": False},
         "band_445": {"datatype": "float", "unit": "AU", "settable": False},
@@ -58,42 +56,42 @@ class SpectrometerReading(BackgroundJobWithDodging):
         "band_680": {"datatype": "float", "unit": "AU", "settable": False},
     }
 
-    def __init__(  # config stuff, settable in activities
-        self,
-        unit,
-        experiment
-    ):
+    def __init__(self, unit, experiment):
         super().__init__(unit=unit, experiment=experiment)
 
-        i2c = board.I2C()
-        self.sensor = adafruit_as7341.AS7341(i2c)
-        
-        self.sensor.led_current = 5
+        try:
+            i2c = board.I2C()
+            self.sensor = adafruit_as7341.AS7341(i2c)
+        except Exception as e:
+            self.logger.error("Is the AS7341 board attached to the Pioreactor HAT?")
+            raise e
+
+        self.sensor.led_current = config.getfloat("spectrometer_reading", "led_current_mA")
 
     def record_band_415(self):
         self.band_415 = self.normalize(self.sensor.channel_415nm)
-        
+
     def record_band_445(self):
         self.band_445 = self.normalize(self.sensor.channel_445nm)
-        
+
     def record_band_480(self):
         self.band_480 = self.normalize(self.sensor.channel_480nm)
-    
+
     def record_band_515(self):
         self.band_515 = self.normalize(self.sensor.channel_515nm)
-        
+
     def record_band_555(self):
         self.band_555 = self.normalize(self.sensor.channel_555nm)
-        
+
     def record_band_590(self):
         self.band_590 = self.normalize(self.sensor.channel_590nm)
-        
+
     def record_band_630(self):
         self.band_630 = self.normalize(self.sensor.channel_630nm)
-        
+
     def record_band_680(self):
         self.band_680 = self.normalize(self.sensor.channel_680nm)
-        
+
     def record_all_bands(self):
         self.record_band_415()
         self.record_band_445()
@@ -103,10 +101,10 @@ class SpectrometerReading(BackgroundJobWithDodging):
         self.record_band_590()
         self.record_band_630()
         self.record_band_680()
-    
+
     def normalize(self, band_recording):
-        return band_recording # / self.sensor.channel_clear
-    
+        return band_recording  # / self.sensor.channel_clear
+
     def action_to_do_before_od_reading(self):
         pass
 
@@ -115,12 +113,13 @@ class SpectrometerReading(BackgroundJobWithDodging):
 
     def turn_off_led(self):
         self.sensor.led = False
-    
+
     def action_to_do_after_od_reading(self):
         self.turn_on_led()
         self.record_all_bands()
         self.turn_off_led()
-        
+
+
 @click.command(name="spectrometer_reading")
 def click_spectrometer_reading():
     """
