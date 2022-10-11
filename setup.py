2eaade920b12b68734229a66e28c5415d2b741bc
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-10-11 12:31:44 -0400

    bump

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from __future__ import annotations
+
+from setuptools import find_packages
+from setuptools import setup
 
 
 setup(
     name="spectrometer_reading_plugin",
-    version="0.0.1",
+    version="0.0.2",
     license="MIT",
     description="Take spectrometer readings (between OD readings) from the Adafruit AS7341 attached to your Pioreactor",
     long_description=open("README.md").read(),
@@ -13,8 +16,6 @@ setup(
     author="Kelly Tran, Cam Davidson-Pilon",
     url="https://github.com/Pioreactor/spectrometer-reading-plugin",
     packages=find_packages(),
-    install_requires=['adafruit-circuitpython-as7341'],
-    entry_points={
-        "pioreactor.plugins": "spectrometer_reading_plugin = spectrometer_reading_plugin"
-    },
+    install_requires=["adafruit-circuitpython-as7341"],
+    entry_points={"pioreactor.plugins": "spectrometer_reading_plugin = spectrometer_reading_plugin"},
 )
