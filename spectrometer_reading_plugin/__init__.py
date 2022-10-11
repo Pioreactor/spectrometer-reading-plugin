1ac804fd565aa3a5f3b47c08e643c2848d983a6c
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-10-11 12:31:26 -0400

    fix

--- spectrometer_reading_plugin/__init__.py
+++ spectrometer_reading_plugin/__init__.py
@@ -1,1 +1,4 @@
-from spectrometer_reading_plugin.relay import click_spectrometer_reading
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from spectrometer_reading_plugin.spectrometer_reading import click_spectrometer_reading  # noqa: F401
