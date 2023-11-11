1251df812c0a34cec652a3806ec2831ed08c9ad7
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-11-11 16:36:34 -0500

    adding views for specific bands, and charts for each one

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name="spectrometer-reading-plugin",
-    version="0.2.7",
+    version="0.2.8",
     license="MIT",
     description="Take spectrometer readings (between OD readings) from the Adafruit AS7341 attached to your Pioreactor",
     long_description=open("README.md").read(),
--- spectrometer_reading_plugin/additional_sql.sql
+++ spectrometer_reading_plugin/additional_sql.sql
@@ -5,3 +5,28 @@ CREATE TABLE IF NOT EXISTS as7341_spectrum_readings (
     reading                  REAL,
     band                     INT
 );
+
+
+CREATE VIEW IF NOT EXISTS as7341_spectrum_readings_415 AS
+  SELECT * FROM as7341_spectrum_readings WHERE band=415;
+
+CREATE VIEW IF NOT EXISTS as7341_spectrum_readings_445 AS
+  SELECT * FROM as7341_spectrum_readings WHERE band=445;
+
+CREATE VIEW IF NOT EXISTS as7341_spectrum_readings_480 AS
+  SELECT * FROM as7341_spectrum_readings WHERE band=480;
+
+CREATE VIEW IF NOT EXISTS as7341_spectrum_readings_515 AS
+  SELECT * FROM as7341_spectrum_readings WHERE band=515;
+
+CREATE VIEW IF NOT EXISTS as7341_spectrum_readings_555 AS
+  SELECT * FROM as7341_spectrum_readings WHERE band=555;
+
+CREATE VIEW IF NOT EXISTS as7341_spectrum_readings_590 AS
+  SELECT * FROM as7341_spectrum_readings WHERE band=590;
+
+CREATE VIEW IF NOT EXISTS as7341_spectrum_readings_630 AS
+  SELECT * FROM as7341_spectrum_readings WHERE band=630;
+
+CREATE VIEW IF NOT EXISTS as7341_spectrum_readings_680 AS
+  SELECT * FROM as7341_spectrum_readings WHERE band=680;
